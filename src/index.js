6b490cd1460b9618b503816668ea211ddb3a1d51
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-11-14 12:00:53 -0800

    add working input and submit for getting weather and outputting to console

--- src/index.js
+++ src/index.js
@@ -1,2 +1,40 @@
 import "./style.css";
 console.log("hello world!");
+
+let app = document.getElementById('app')
+
+
+let zipInput = document.createElement('input')
+zipInput.type = 'text'
+zipInput.placeholder = 'e.g.: 94108'
+
+let zipInputButton = document.createElement('button')
+zipInputButton.textContent = 'Submit'
+
+app.append(zipInput, zipInputButton)
+
+zipInputButton.addEventListener('click', event => {
+  event.preventDefault()
+
+  getWeather(zipInput.value).then(result => {
+    // console.log(result)
+  })
+})
+
+async function getWeather(zip) {
+  try {
+    const data = await getZipInfo(zip)
+    console.log(`https://api.openweathermap.org/data/2.5/weather?lat=${data.lat}&lon=${data.lon}&appid=b43956d89a79c4536461499aee6eb38b&units=imperial`)
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${data.lat}&lon=${data.lon}&appid=b43956d89a79c4536461499aee6eb38b&units=imperial`, {mode: 'cors'})
+    const weatherData = await response.json()
+    console.log(`${weatherData.main.temp} degrees fahrenheit`)
+  } catch (err) {
+    alert(err)
+  }
+}
+
+async function getZipInfo(zip) {
+  const response = await fetch(`https://api.openweathermap.org/geo/1.0/zip?zip=${zip},US&appid=b43956d89a79c4536461499aee6eb38b&units=imperial`, {mode: 'cors'})
+  const apiData = await response.json()
+  return apiData
+}
