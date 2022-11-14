9d2b6fa9fc7cdbdd25da1c950851b45c3252226f
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-11-14 12:10:21 -0800

    async displayWeather function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { data } from "autoprefixer";
 import "./style.css";
 console.log("hello world!");
 
@@ -28,6 +29,7 @@ async function getWeather(zip) {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${data.lat}&lon=${data.lon}&appid=b43956d89a79c4536461499aee6eb38b&units=imperial`, {mode: 'cors'})
     const weatherData = await response.json()
     console.log(`${weatherData.main.temp} degrees fahrenheit`)
+    displayWeather(weatherData)
   } catch (err) {
     alert(err)
   }
@@ -37,4 +39,13 @@ async function getZipInfo(zip) {
   const response = await fetch(`https://api.openweathermap.org/geo/1.0/zip?zip=${zip},US&appid=b43956d89a79c4536461499aee6eb38b&units=imperial`, {mode: 'cors'})
   const apiData = await response.json()
   return apiData
+}
+
+async function displayWeather(data) {
+  // console.log(data.main.temp)
+  let tempValue = await data.main.temp
+  console.log(tempValue)
+  let tempElem = document.createElement('h1')
+  tempElem.textContent = tempValue
+  app.append(tempElem)
 }
